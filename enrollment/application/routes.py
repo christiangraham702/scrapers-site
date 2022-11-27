8618a3d13360799bdf92b968970220369dea9f6d
christiangraham702 <christian.graham@ufl.edu>
2022-11-27 02:17:36 -0500

    added page to get user email

--- enrollment/application/routes.py
+++ enrollment/application/routes.py
@@ -1,5 +1,10 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, Response, json
+
+
+job_data = [{"jobID":"01", "website":"test.com", "query":"orange", "item_count":"5839", "date":"042021", "send_data":"email me"},
+                {"jobID":"02", "website":"test.com", "query":"orange", "item_count":"5839", "date":"042021", "send_data":"email me"},
+                {"jobID":"03", "website":"test.com", "query":"orange", "item_count":"5839", "date":"042021", "send_data":"email me"}]
 
 
 @app.route('/')
@@ -25,10 +30,26 @@ def login():
     return render_template('login.html', login=True)
 
 @app.route('/jobs')
-def jobs():
-    job_data = [{"jobID":"01", "website":"test.com", "query":"orange", "items":"5839", "date":"042021", "send_data":"email me"},
-                {"jobID":"02", "website":"test.com", "query":"orange", "items":"5839", "date":"042021", "send_data":"email me"},
-                {"jobID":"03", "website":"test.com", "query":"orange", "items":"5839", "date":"042021", "send_data":"email me"}]
-    print(job_data[0]['jobID'])
-    return render_template('courses.html', job_data=job_data)
+@app.route('/jobs/<spider>') # <spider> is a variable, to pass to the template, here it is the name of the spider
+def jobs(spider="all"): #defaults to showing all jobs
+    return render_template('courses.html', job_data=job_data, spider=spider) #so we can use it in the template
 
+@app.route('/send_data', methods=['POST', 'GET'])
+def send_data():
+    id = request.form.get('jobID')
+    website = request.form.get('website')
+    query = request.form.get('query')
+    item_count = request.form.get('item_count')
+    date = request.form.get('date')
+    spider = request.form.get('spider')
+    return render_template('send_data.html', send_data=True, data = {"jobID":id, "website":website, "query":query, "item_count":item_count, "date":date, "spider":spider})
+
+@app.route('/api/')
+@app.route('/api/<idx>')
+def api(idx=None):
+    if idx == None:
+        json_data = job_data
+    else:
+        json_data = job_data[int(idx)]
+
+    return Response(json.dumps(json_data), mimetype='application/json')
